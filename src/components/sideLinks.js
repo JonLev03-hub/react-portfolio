6d30ce60054185b6511e9977bf6aac5c32b32d3c
Jonathan Leverenz <jonlev03@gmail.com>
2022-05-08 14:05:18 -0400

    foxed side bar position

--- src/components/sideLinks.js
+++ src/components/sideLinks.js
@@ -1,29 +1,27 @@
-import SideLinksCss from './sideLinks.module.css';
+import styles from './sideLinks.module.css';
 export default function SideLinks() {
     const links = [
         {
             name: "linkedin",
             url: "https://www.linkedin.com/in/JonathanMLeverenz",
-            image: "../assets/linkedin-icon.png"
+            src: require("../assets/linkedin-icon.png")
         },
         {
             name: "github",
             url: "https://www.github.com/jonlev03-hub",
-            image: "../assets/github-icon.png"
+            src: require("../assets/github-icon.png")
         },
     ]
     return (
-        <div className="side-links">
-            <ul>
-                {links.map(link => (
-                    <li key={link.name}>
-                        <a href={link.url} target="_blank" rel="noopener noreferrer">
-                            <img src={link.image} alt={link.name} />
+            <ul className = {styles.ul}>
+                {links.map(link => 
+                    <li key = {link.name} className = {styles.li}>
+                        <a>
+                            <img className = {styles.img} src={link.src} alt={link.name}/>
                         </a>
                     </li>
-                ))}
+                )}
             </ul>
-        </div>
     );
 }
 
