c3ff53ed211590a2426c44233075f1af55aa2b1a
Jonathan Leverenz <jonlev03@gmail.com>
2022-05-09 11:45:38 -0400

    misc fixes

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -1,10 +1,24 @@
-export default function Skills() {
-    return (
-        <div className = "skills" >
-            <h1>Skills</h1>
-            <p>
-                lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>
-        </div>
-    )
+import SideLinks from "../components/sideLinks";
+import Article from "../components/article";
+import { Link } from "react-router-dom";
+export default function About() {
+  return (
+    <div className="about">
+      <SideLinks />
+      <Article>
+        <h1>
+          const <white>skills</white> = () =&gt; &#123;
+          <br />
+        </h1>
+        <p>
+          const skills = &#123;  "Languages" : [ "Python" , "Javascript",
+          "HTML/Css", "MySQL"]  "General" : ["AWS"]  "Data-analytics" :
+          ["Pandas", "Tensorflow", "Tableau", "Statistics"]  "Web-development" :
+          ["React", "Redux",]  "Soft-skills" : ["Problem Solving", "Learning",
+          "Communication"] &#125;; console.log(skills);
+        </p>
+        <h1>&#125;;</h1>
+      </Article>
+    </div>
+  );
 }
