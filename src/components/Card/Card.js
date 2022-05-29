d9262cb8fb717e0056a385f9709801f5c007e5be
Jonathan Leverenz <jonlev03@gmail.com>
2022-05-29 18:14:23 -0400

    fully responsive cards:

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,27 +5,33 @@ export default function Card(props) {
     <div className={Styles.card} tabIndex="0">
       <div className={Styles.sample}>
         <img src={props.img} alt="sample" className={Styles.img} />
-        <h1>{props.title}</h1>
-        <a href={props.link} target="_blank" rel="noopener noreferrer">
-          Link
-        </a>
-        <a href={props.github} target="_blank" rel="noopener noreferrer">
-          Github
-        </a>
-        <ul className={Styles.techList}>
-          {props.tech.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
-        <p>{props.description}</p>
+        <div className={Styles.textContainer}>
+          <h1>{props.title}</h1>
+          <ul className={Styles.techList}>
+            {props.tech.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+          <p>{props.description}</p>
+          <div className={Styles.links}>
+            <a href={props.link} target="_blank" rel="noopener noreferrer">
+              Visit Site
+            </a>
+            <a href={props.github} target="_blank" rel="noopener noreferrer">
+              Github
+            </a>
+          </div>
+        </div>
       </div>
       <div className={Styles.extra}>
-        {props.extra.map((item, index) => (
-          <div>
-            <h3>{item.title}</h3>
-            <p>{item.content}</p>
-          </div>
-        ))}
+        <div className={Styles.textContainer}>
+          {props.extra.map((item, index) => (
+            <>
+              <h3>{item.title}</h3>
+              <p>{item.content}</p>
+            </>
+          ))}
+        </div>
       </div>
     </div>
   );
