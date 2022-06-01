5da845319f8f68708b8faae6e05ade391817b224
Jonathan Leverenz <jonlev03@gmail.com>
2022-05-31 21:35:05 -0400

    added new project

--- src/pages/Work/Work.js
+++ src/pages/Work/Work.js
@@ -19,7 +19,7 @@ export default function Work() {
         {
           title: "Problems :",
           content:
-            "The development of this site had run relatively smoothly since it is a small project but the alrgest problem that had occured was designing the site. With a website like this your goal is to display the work of the client as best as possible but when working with this client they did not have much content to display yet. The current design was made to be able to easily grow with the client and with minor adjustment you can make the website display large portfolios with ease.",
+            "The development of this site had run relatively smoothly since it is a small project but the largest problem that had occured was designing the site. With a website like this your goal is to display the work of the client as best as possible but when working with this client they did not have much content to display yet. The current design was made to be able to easily grow with the client and with minor adjustment you can make the website display large portfolios with ease.",
         },
       ],
     },
@@ -41,6 +41,27 @@ export default function Work() {
           title: "Problems :",
           content:
             "One of the main problems when building this site has been the design and layout. Most of the websites I have built so far have been relatively streight forward but when I started this project I hadn't had much experience creating the content for sites. I soon noticed building a design that is simple or already made is the easy part. The hard part is finding what parts can go together and look amazing in the process.",
+        },
+      ],
+    },
+    {
+      img: "./images/portfolio.png",
+      title: "My old portfolio",
+      description:
+        "This is one of the first projects I had made in web development. This is a portfolio created in Html Css and Javascript that pulls your projects in from github and displays them along with some information about you.",
+      link: "jonlev03-hub.github.io/portfolio/",
+      github: "https://github.com/jonlev03-hub/portfolio",
+      tech: ["Javascript", "HTML", "CSS"],
+      extra: [
+        {
+          title: "My Responsibilities :",
+          content:
+            "I am designing and building this project is being designed and built by myself. I will be responsible for all front end design, and implimentation.",
+        },
+        {
+          title: "Problems :",
+          content:
+            "Durring this project I had struggled a lot with design and the use of color. I had been new to web development and design since this project was done when I was studying to be a data scientist. This website also has some issues reguarding best practice for html and has accessibility conserns. I likely won't revisit this project to fix these issues but I believe that it is still a good part of the learning process I endured.",
         },
       ],
     },
